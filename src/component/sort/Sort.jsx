49f7bfc2d4380b92edce04aea351bf72db93c0ba
smoothwarhead <simeont126@outlook.com>
2023-12-13 03:35:10 -0600

    latest commit

--- src/component/sort/Sort.jsx
+++ src/component/sort/Sort.jsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import SortItems from "./sortitems/SortItems";
 import "./sort.css";
 import Button from "../button/Button";
+import { ViewContext } from "../../context/ViewContext";
+
+
+
 
 
 function Sort({ setSlideSort }) {
+
+	const { mobile } = useContext(ViewContext)
+
 	return (
 		<div className="sort">
 			<SortItems />
 			
-			<Button
-				action={() => setSlideSort(false)}
-				cName="sort-done-btn"
-				name="Done"
+			{mobile &&
+				<Button
+					action={() => setSlideSort(false)}
+					cName="sort-done-btn"
+					name="Done"
 
-			/>
+				/>
+			}
 		</div>
 	);
 }
